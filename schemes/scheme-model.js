2be56237573de692d2c806fb1cc6163d219f35f7
Dave Irwin <david@redirwin.com>
2019-08-14 14:38:49 -0700

    GET steps by id with findSteps, working, but also returning scheme_id for some reason.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require("../data/db.config");
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 };
 
 function find() {
@@ -11,4 +12,22 @@ function find() {
 
 function findById(id) {
   return db("schemes").where({ id });
+}
+
+function findSteps(id) {
+  /*
+select schemes.scheme_name, steps.step_number, steps.instructions
+from schemes
+join steps on steps.scheme_id = schemes.id
+where schemes.id = 2
+order by step_number
+*/
+
+  // WHY IS THIS RETURNING SCHEME_ID ???
+
+  return db("schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .from("schemes")
+    .join("steps", "steps.scheme_id", "=", "schemes.id")
+    .where({ "schemes.id": id })
+    .orderBy("step_number");
 }
